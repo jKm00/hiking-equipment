3a0c10d588532d6dc928532c4f4a247e80db97de
Joakim <joakied@ntnu.no>
2022-03-10 12:00:33 +0100

    Fix react Not really

--- public_html/script.js
+++ public_html/script.js
@@ -1,6 +1,21 @@
-const toggleSearchBtn = document.querySelector("[data-search-toggle]")
-const searchForm = document.querySelector("[data-search-form]")
+const API_URL = "http://localhost:8080";
 
-toggleSearchBtn.addEventListener("click", e => {
-    searchForm.classList.toggle("hidden")
+const toggleSearchBtn = document.querySelector("[data-search-toggle]");
+const searchForm = document.querySelector("[data-search-form]");
+
+toggleSearchBtn.addEventListener("click", (e) => {
+  searchForm.classList.toggle("hidden");
+});
+
+fetch(API_URL + "/products", {
+  method: "Get",
 })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    console.log(json);
+  })
+  .catch(function (err) {
+    alert("Error: " + err);
+  });
