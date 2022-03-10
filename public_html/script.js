a368496e54af5337537157df9ce7a6993daadd0d
Joakim <joakied@ntnu.no>
2022-03-10 12:41:19 +0100

    BUG FIX: search bar toggle

--- public_html/script.js
+++ public_html/script.js
@@ -1,7 +1,15 @@
 const API_URL = "http://localhost:8080";
 
+const page = document.querySelector("body");
 const toggleSearchBtn = document.querySelector("[data-search-toggle]");
 const searchForm = document.querySelector("[data-search-form]");
+
+page.addEventListener("click", (e) => {
+  const result = e.target;
+  if (!result.hasAttribute("data-search-icon")) {
+    searchForm.classList.add("hidden");
+  }
+});
 
 toggleSearchBtn.addEventListener("click", (e) => {
   searchForm.classList.toggle("hidden");
