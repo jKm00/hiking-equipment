caa66b475385ccc581b5d1c57b29d7db4543ebfc
Joakim <joakimedvardsen2000@gmail.com>
2022-03-11 23:50:35 +0100

    Added some temp styles to product page

--- webapp/src/pages/ProductPage.jsx
+++ webapp/src/pages/ProductPage.jsx
@@ -4,8 +4,13 @@ import { useParams } from "react-router-dom";
 function ProductPage() {
   const { id } = useParams();
 
+  const mystyle = {
+    gridColumn: "2 / -2",
+    margin: "12.8em 0",
+  };
+
   return (
-    <div style={{ gridColumn: "1 / -1" }}>
+    <div style={mystyle}>
       <h1>Product page</h1>
       <p>
         Tried to display item with id {id}, but failed because this page is not
