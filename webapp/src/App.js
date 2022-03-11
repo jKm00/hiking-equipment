961d25795ce57b8d98d6efc3a8789094b8a308d6
Joakim <joakimedvardsen2000@gmail.com>
2022-03-11 01:18:41 +0100

    Added navbar and footer component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<h1>Home Page</h1>} />
+        <Route path="/shop" element={<h1>Shop page</h1>} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
