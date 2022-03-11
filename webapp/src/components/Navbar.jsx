72e470f6c583572443a02446a3e241f4c08644ff
Joakim <joakimedvardsen2000@gmail.com>
2022-03-11 01:26:19 +0100

    Added search bar toggle

--- webapp/src/components/Navbar.jsx
+++ webapp/src/components/Navbar.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  function doToggleSearch() {
+    const searchBar = document.querySelector("[data-search-form]");
+    searchBar.classList.toggle("hidden");
+  }
+
   return (
     <nav className="nav">
       <div className="first__nav">
@@ -10,16 +15,9 @@ function Navbar() {
         </Link>
         <ul className="first__nav__icons">
           <li className="first__nav__item">
-            <button className="btn" data-search data-search-toggle>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                data-search
-              >
-                <path
-                  data-search
-                  d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"
-                />
+            <button className="btn" onClick={doToggleSearch}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
               </svg>
             </button>
           </li>
@@ -46,29 +44,16 @@ function Navbar() {
           </li>
         </ul>
       </div>
-      <form
-        action=""
-        className="nav__search-bar hidden"
-        data-search
-        data-search-form
-      >
+      <form action="" className="nav__search-bar hidden" data-search-form>
         <div className="nav__search-bar--wrapper">
           <input
             type="text"
             placeholder="Search..."
             className="nav__search-bar__input"
-            data-search
           />
           <button className="btn btn--search" data-search>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              data-search
-            >
-              <path
-                data-search
-                d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
             </svg>
           </button>
         </div>
