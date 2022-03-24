be599e8dd882265097f126aa705fa8b0a2c7404d
Eduard Andrei Cristea <falniir@gmail.com>
2022-03-24 15:37:12 +0100

    added text and button to carousel header slides

--- webapp/src/components/Header.jsx
+++ webapp/src/components/Header.jsx
@@ -6,15 +6,24 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 
-export default function App() {
+export default function Header() {
   return (
     <>
 
       <Swiper className="mySwiper header">
       
 
-        <SwiperSlide> Test
+        <SwiperSlide className="header-carousel">
         <img className="retard" src="https://picsum.photos/1920/1080" alt="img" />
+        <div
+        class="text"
+        data-swiper-parallax="-300"
+        data-swiper-parallax-duration="600"
+      >
+       <button className="cta">Go to shop</button>
+        <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
+      </div>
+
         </SwiperSlide>
         <SwiperSlide><img src="https://picsum.photos/1920/1080" alt="img" /></SwiperSlide>
         <SwiperSlide><img src="https://picsum.photos/1920/1080" alt="img" /></SwiperSlide>
