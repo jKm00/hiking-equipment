af50c72d1e34432c5a9ed39f754884c5807f7ad3
Eduard Andrei Cristea <falniir@gmail.com>
2022-03-28 13:07:41 +0200

    Reconfigured styles for header and fixed positions

--- webapp/src/components/Header.jsx
+++ webapp/src/components/Header.jsx
@@ -10,13 +10,13 @@ export default function Header() {
   return (
     <>
 
-      <Swiper className="mySwiper header">
+      <Swiper className="header">
       
 
-        <SwiperSlide className="header-carousel">
-        <img className="retard" src="https://picsum.photos/1920/1080" alt="img" />
+        <SwiperSlide className="header-carousel__item">
+        <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080" alt="img" />
         <div
-        class="text"
+        className="header--wrapper"
         data-swiper-parallax="-300"
         data-swiper-parallax-duration="600"
       >
@@ -24,12 +24,13 @@ export default function Header() {
         <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
       </div>
 
+        //TO DO: ADD CLASSES TO FOLLOWING SWIPER TAGS
         </SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080" alt="img" /></SwiperSlide>
+        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=1" alt="img" /></SwiperSlide>
+        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=2" alt="img" /></SwiperSlide>
+        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=3" alt="img" /></SwiperSlide>
+        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=4" alt="img" /></SwiperSlide>
+        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=5" alt="img" /></SwiperSlide>
       </Swiper>
     </>
   );
