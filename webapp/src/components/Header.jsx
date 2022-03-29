1a310d943e40348795d62c9148c2e869e618f565
Eduard Andrei Cristea <falniir@gmail.com>
2022-03-29 21:57:22 +0200

    Added Navigation, Navbar and Autoplay to carousel

--- webapp/src/components/Header.jsx
+++ webapp/src/components/Header.jsx
@@ -1,17 +1,30 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 
 export default function Header() {
   return (
     <>
 
-      <Swiper className="header">
-      
+      <Swiper className="header"
+              autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              >
 
         <SwiperSlide className="header-carousel__item">
         <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080" alt="img" />
@@ -23,14 +36,82 @@ export default function Header() {
        <button className="cta">Go to shop</button>
         <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
       </div>
-
-        //TO DO: ADD CLASSES TO FOLLOWING SWIPER TAGS
         </SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=1" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=2" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=3" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=4" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/1920/1080?random=5" alt="img" /></SwiperSlide>
+
+        <SwiperSlide className="header-carousel__item">
+        <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080?random=1" alt="img" />
+        <div
+        className="header--wrapper"
+        data-swiper-parallax="-300"
+        data-swiper-parallax-duration="600"
+      >
+       <button className="cta">Go to shop</button>
+        <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
+      </div>
+        </SwiperSlide>
+        <SwiperSlide className="header-carousel__item">
+        <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080?random=2" alt="img" />
+        <div
+        className="header--wrapper"
+        data-swiper-parallax="-300"
+        data-swiper-parallax-duration="600"
+      >
+       <button className="cta">Go to shop</button>
+        <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
+      </div>
+        </SwiperSlide>
+        
+
+        <SwiperSlide className="header-carousel__item">
+        <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080?random=3" alt="img" />
+        <div
+        className="header--wrapper"
+        data-swiper-parallax="-300"
+        data-swiper-parallax-duration="600"
+      >
+       <button className="cta">Go to shop</button>
+        <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide className="header-carousel__item">
+        <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080?random=4" alt="img" />
+        <div
+        className="header--wrapper"
+        data-swiper-parallax="-300"
+        data-swiper-parallax-duration="600"
+      >
+       <button className="cta">Go to shop</button>
+        <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide className="header-carousel__item">
+        <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080?random=5" alt="img" />
+        <div
+        className="header--wrapper"
+        data-swiper-parallax="-300"
+        data-swiper-parallax-duration="600"
+      >
+       <button className="cta">Go to shop</button>
+        <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide className="header-carousel__item">
+        <img className="header-carousel-item-wrapper" src="https://picsum.photos/1920/1080?random=6" alt="img" />
+        <div
+        className="header--wrapper"
+        data-swiper-parallax="-300"
+        data-swiper-parallax-duration="600"
+      >
+       <button className="cta">Go to shop</button>
+        <h1 className="header-title">Lorem ipsum dolor sit amet, ...</h1>
+      </div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
