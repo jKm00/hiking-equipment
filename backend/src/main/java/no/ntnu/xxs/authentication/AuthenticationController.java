01eda01f135172cb8753b45c3ebec259223158e6
Joakim <joakied@stud.ntnu.no>
2022-04-14 23:18:21 +0200

    Started on login handler

--- backend/src/main/java/no/ntnu/xxs/authentication/AuthenticationController.java
+++ backend/src/main/java/no/ntnu/xxs/authentication/AuthenticationController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,7 @@ public class AuthenticationController {
      * @return Http.OK with JWT token, or Http. UNAUTHORIZED
      */
     @PostMapping("/api/authenticate")
+    @CrossOrigin
     public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
--- frontend/webapp/src/pages/LoginPage.jsx
+++ frontend/webapp/src/pages/LoginPage.jsx
@@ -4,9 +4,50 @@ import "../styles/login.css";
 import "../styles/form.css";
 
 export default function LoginPage() {
+  const LOGIN_URL = "http://localhost:8080/api/authenticate";
+
+  /**
+   * Sends a login request to the API
+   * @param {*} userDetails the username and password of the user to login
+   */
+  async function loginUser(userDetails) {
+    console.log(JSON.stringify(userDetails));
+    const data = await fetch(LOGIN_URL, {
+      method: "POST",
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userDetails),
+    });
+    return await data.json();
+  }
+
+  /**
+   * Handles the event when the login button is presses
+   * @param {*} event
+   */
+  function handleSubmit(event) {
+    // Prevent form from defaul behavior
+    event.preventDefault();
+    // Create authentication request from input values
+    const requestData = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    // Send login request to API
+    // TODO: fix 416 http response code
+    const response = loginUser(requestData);
+    // Check if response contains a JWT token
+    if ("accessToken" in response) {
+      console.log("Logged in");
+    } else {
+      console.log("Login failed!");
+    }
+  }
+
   return (
     <div className="login--background">
-      <form action="" className="form">
+      <form action="" method="POST" className="form" onSubmit={handleSubmit}>
         <Link to="/" className="form__logo logo">
           XXS
         </Link>
@@ -16,13 +57,24 @@ export default function LoginPage() {
             <label htmlFor="username" className="form__label">
               Username / email
             </label>
-            <input type="text" id="username" className="form__input" required />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="form__input"
+              required
+            />
           </div>
           <div className="form__input--wrapper">
             <label htmlFor="password" className="form__label">
               Password
             </label>
-            <input type="password" id="password" className="form__input" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="form__input"
+            />
           </div>
         </fieldset>
         <button className="cta cta--max-width">Login</button>
