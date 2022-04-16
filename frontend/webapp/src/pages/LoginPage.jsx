eaa72934b96c842d7ae6c21d0d8ca0d3459cc4e4
Joakim <joakied@stud.ntnu.no>
2022-04-16 17:32:24 +0200

    Finished logic for logging in

--- frontend/webapp/src/pages/LoginPage.jsx
+++ frontend/webapp/src/pages/LoginPage.jsx
@@ -1,25 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import "../styles/login.css";
 import "../styles/form.css";
 
 export default function LoginPage() {
   const LOGIN_URL = "http://localhost:8080/api/authenticate";
-
+  const navigate = useNavigate();
   /**
    * Sends a login request to the API
    * @param {*} userDetails the username and password of the user to login
    */
-  async function loginUser(userDetails) {
+  async function sendLoginRequest(userDetails) {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", LOGIN_URL);
     xhr.setRequestHeader("Content-Type", "application/json");
+
     xhr.onreadystatechange = function () {
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        console.log(this.response);
-        return this.response;
+      if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200) {
+          // Save jwt to browser
+          const result = JSON.parse(this.response);
+          localStorage.setItem("jwt", result.jwt);
+          navigate("/");
+        } else if (this.status === 401) {
+          // Display error message
+          const errorMsg = document.querySelector("[data-error]");
+          console.log(errorMsg);
+          errorMsg.style.opacity = 1;
+        }
       }
     };
+
     xhr.send(JSON.stringify(userDetails));
   }
 
@@ -36,17 +47,7 @@ export default function LoginPage() {
       password: event.target.password.value,
     };
     // Send login request to API
-    // TODO: fix response being undefined
-    const response = loginUser(requestData);
-    console.log(response);
-    // Check if response contains a JWT token
-    if ("jwt" in response) {
-      // TODO: Store JWT token in browser
-      console.log("Logged in");
-    } else {
-      // TODO: Display error msg
-      console.log("Login failed!");
-    }
+    sendLoginRequest(requestData);
   }
 
   return (
@@ -80,6 +81,9 @@ export default function LoginPage() {
               className="form__input"
             />
           </div>
+          <p className="form__error-msg" data-error>
+            Something went wrong, please try again...
+          </p>
         </fieldset>
         <button className="cta cta--max-width">Login</button>
         <p className="login__redirect">
