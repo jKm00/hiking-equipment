51ddd57ce6069cb848df09622caeb115a1e54ffc
Joakim <joakied@stud.ntnu.no>
2022-04-28 09:14:58 +0200

    Created a send api request function
    Should be used for all request sent to the backedn

--- frontend/webapp/src/pages/LoginPage.jsx
+++ frontend/webapp/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
+import { sendApiRequest } from "../tools/request";
 
 import "../styles/login.css";
 import "../styles/form.css";
---
+++ frontend/webapp/src/tools/cookies.js
@@ -0,0 +1,20 @@
+/**
+ * Get calue of a specific cookie
+ * @param {*} cookieName Key of the cookie to get
+ * @return {string} Value of cookie or "" if cookie is not found
+ */
+export function getCookie(cookieName) {
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
