eb4aa9fa365559b22b0f765a718b8838da7974cf
Joakim <joakied@stud.ntnu.no>
2022-04-28 09:18:50 +0200

    Implemented add and delete cookie functions

--- frontend/webapp/src/tools/cookies.js
+++ frontend/webapp/src/tools/cookies.js
@@ -18,3 +18,24 @@ export function getCookie(cookieName) {
   }
   return "";
 }
+
+/**
+ * Store a cookie to the browser
+ * @param {*} cookieName name/key of the cookie
+ * @param {*} cookieValue value of the cookie
+ * @param {*} exdays expiry time in days
+ */
+export function setCookie(cookieName, cookieValue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+/**
+ * Delete a cookie
+ * @param cookieName Name of the cookie to delete
+ */
+export function deleteCookie(cookieName) {
+  setCookie(cookieName, "", -1);
+}
