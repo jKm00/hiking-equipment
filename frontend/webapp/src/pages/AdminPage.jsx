16b236e47524eb5b0a557632c1f5e81b7ec8d418
jKm00 <joakied@stud.ntnu.no>
2022-05-01 15:34:51 +0200

    Dashboardified admin page

--- frontend/webapp/src/pages/AdminPage.jsx
+++ frontend/webapp/src/pages/AdminPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import PictureForm from "../components/PictureForm";
 import Footer from "../components/Footer";
 
 import "../styles/form.css";
@@ -9,6 +10,7 @@ import "../styles/admin.css";
 export default function AdminPage({ user }) {
   const navigate = useNavigate();
 
+  // Redirects none admins back to main page
   useEffect(() => {
     if (!user || !user.roles.includes("ROLE_ADMIN")) {
       navigate("/");
@@ -18,34 +20,7 @@ export default function AdminPage({ user }) {
   return (
     <>
       <div className="admin-page">
-        <form action="" className="form">
-          <fieldset className="form__section">
-            <legend className="form__title form__title--small">
-              Add picture to product
-            </legend>
-            <div className="form__input--wrapper">
-              <label htmlFor="product" className="form__label">
-                Product to add image to
-              </label>
-              <select name="product" id="product" className="form__input">
-                <option value="1">Winter Sweater</option>
-                <option value="2">Hiking Boots</option>
-              </select>
-            </div>
-            <div className="form__input--wrapper">
-              <label htmlFor="product-image" className="form__label">
-                Product image:
-              </label>
-              <input
-                type="file"
-                id="product-image"
-                name="product-image"
-                accept="image/png, image/jpeg"
-                className="form__input"
-              />
-            </div>
-          </fieldset>
-        </form>
+        <PictureForm />
       </div>
       <Footer />
     </>
