4c14ad17089600a8b14485861c941e324652bc65
jKm00 <joakied@stud.ntnu.no>
2022-05-03 19:37:32 +0200

    Added inputs to specify colors, sizes and images when adding product from admin page

--- frontend/webapp/src/components/ProductForm.jsx
+++ frontend/webapp/src/components/ProductForm.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 import "../styles/productForm.css";
 import "../styles/table.css";
 
 export default function ProductForm({ products }) {
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [sex, setSex] = useState("");
+  const [colors, setColors] = useState("");
+  const [sizes, setSize] = useState("");
+  const [images, setImages] = useState([]);
+
   /**
    * TODO: Handles adding products
    */
   function handleSubmit(event) {
     event.preventDefault();
+    console.log(images);
   }
 
   /**
@@ -28,36 +40,105 @@ export default function ProductForm({ products }) {
             <label htmlFor="product-name" className="form__label">
               Name
             </label>
-            <input id="product-name" type="text" className="form__input" />
+            <input
+              id="product-name"
+              type="text"
+              className="form__input"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="form__input--wrapper">
             <label htmlFor="product-name" className="form__label">
               Desc
             </label>
-            <input id="product-name" type="text" className="form__input" />
+            <input
+              id="product-name"
+              type="text"
+              className="form__input"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
           </div>
           <div className="form__input--wrapper">
             <label htmlFor="product-name" className="form__label">
               Price
             </label>
-            <input id="product-name" type="number" className="form__input" />
+            <input
+              id="product-name"
+              type="number"
+              className="form__input"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
           </div>
           <div className="form__input--wrapper">
             <label htmlFor="product-name" className="form__label">
               Category
             </label>
-            <input id="product-name" type="text" className="form__input" />
+            <input
+              id="product-name"
+              type="text"
+              className="form__input"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
           </div>
           <div className="form__input--wrapper">
             <label htmlFor="product-name" className="form__label">
               Sex
             </label>
-            <input id="product-name" type="text" className="form__input" />
+            <input
+              id="product-name"
+              type="text"
+              className="form__input"
+              value={sex}
+              onChange={(e) => setSex(e.target.value)}
+            />
           </div>
-          <button className="cta cta--tiny" onClick={handleSubmit}>
-            Add product
-          </button>
         </fieldset>
+        <fieldset className="form__section form__section--no-space form__section--horizontal">
+          <div className="form__input--wrapper">
+            <label htmlFor="product-color" className="form__label">
+              Colors (separeted by ", ")
+            </label>
+            <input
+              id="product-color"
+              type="text"
+              className="form__input"
+              value={colors}
+              onChange={(e) => setColors(e.target.value)}
+            />
+          </div>
+          <div className="form__input--wrapper">
+            <label htmlFor="product-sizes" className="form__label">
+              Sizes (separeted by ", ")
+            </label>
+            <input
+              id="product-sizes"
+              type="text"
+              className="form__input"
+              value={sizes}
+              onChange={(e) => setSize(e.target.value)}
+            />
+          </div>
+          <div className="form__input--wrapper">
+            <label htmlFor="product-images" className="form__label">
+              Images
+            </label>
+            <input
+              id="product-images"
+              type="file"
+              className="form__input form__input--file"
+              accept="image/png, image/jpeg"
+              multiple
+              onChange={(e) => setImages(e.target.files)}
+            />
+          </div>
+        </fieldset>
+        <button className="cta cta--tiny" onClick={handleSubmit}>
+          Add product
+        </button>
       </form>
       <table className="table">
         <caption className="table__title">Products</caption>
--- frontend/webapp/src/pages/AdminPage.jsx
+++ frontend/webapp/src/pages/AdminPage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { sendApiRequest } from "../tools/request";
 
 import ProductForm from "../components/ProductForm";
@@ -10,7 +9,6 @@ import "../styles/form.css";
 import "../styles/admin.css";
 
 export default function AdminPage({ user }) {
-  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
 
   // Load products
