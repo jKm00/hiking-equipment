c7902e69e0ccf3c72cb56302a12d6bdcc98cb98a
Torstein Eide <taurstein@gmail.com>
2022-05-05 09:41:59 +0200

    Started implementing relation correctly

---
+++ backend/src/main/java/no/ntnu/xxs/entities/orders/OrderProducts.java
@@ -0,0 +1,33 @@
+package no.ntnu.xxs.entities;
+
+import no.ntnu.xxs.product.Product;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * This class represents the products in an order in the web shop
+ */
+@Entity
+@Table(name = "order_products")
+public class OrderProducts implements Serializable
+{
+    @EmbeddedId
+    OrderProductsKey id;
+
+    @ManyToOne
+    @MapsId("orderID")
+    @JoinColumn(name = "order_id")
+    private Orders order;
+
+
+    @ManyToOne
+    @MapsId("productID")
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    /**
+     * Empty constructor
+     */
+    public OrderProducts(){}
+}
--- backend/src/main/java/no/ntnu/xxs/entities/orders/Orders.java
+++ backend/src/main/java/no/ntnu/xxs/entities/orders/Orders.java
@@ -1,6 +1,8 @@
 package no.ntnu.xxs.entities;
 
 
+import no.ntnu.xxs.user.User;
+
 import javax.persistence.*;
 
 /**
@@ -15,6 +17,12 @@ public class Orders
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(unique = true, name = "order_id")
     private long id;
+
+    //Foreign key
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Column(name = "total_price")
     private long totalPrice;
     @Column(name = "status")
@@ -28,4 +36,19 @@ public class Orders
 
     }
 
+    public long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(long totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
