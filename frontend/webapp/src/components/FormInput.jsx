5396c2cedac4b1e9c34e0afad86c93574d897aa6
jKm00 <joakied@stud.ntnu.no>
2022-05-16 19:27:20 +0200

    Refactored add product form

---
+++ frontend/webapp/src/components/FormInput.jsx
@@ -0,0 +1,25 @@
+export default function FormInput({ value, updateValue, label, type }) {
+  function handleChange(e) {
+    if (type !== null && type === "checkbox") {
+      updateValue(!value);
+    } else {
+      updateValue(e.target.value);
+    }
+  }
+
+  return (
+    <div className="form__input--wrapper">
+      <label htmlFor={label} className="form__label">
+        {label}
+      </label>
+      <input
+        id={label}
+        type={type == null ? "text" : type}
+        accept={type !== null && type === "file" ? "image/png, image/jpeg" : ""}
+        className="form__input"
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
--- frontend/webapp/src/components/admin/ProductForm.jsx
+++ frontend/webapp/src/components/admin/ProductForm.jsx
@@ -3,323 +3,100 @@ import { sendApiRequest } from "../../tools/request";
 import { displayFeedback } from "../../tools/feedback";
 import { isImages } from "../../tools/validators";
 
+import FormInput from "../FormInput";
+
 import "../../styles/productForm.css";
 import "../../styles/table.css";
 
 export default function ProductForm({ products, updateProducts }) {
-  const [name, setName] = useState("");
-  const [desc, setDesc] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [sex, setSex] = useState("");
-  const [colors, setColors] = useState("");
-  const [sizes, setSize] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [images, setImages] = useState([]);
   const [discount, setDiscount] = useState("");
+  const [colors, setColors] = useState("");
+  const [sizes, setSizes] = useState("");
+  const [images, setImages] = useState("");
+  const [featured, setFeatured] = useState(false);
   const [details, setDetails] = useState("");
 
-  const [discounts, setDiscounts] = useState([]);
-
-  // Retrieve all the discount stored in the backend
-  useEffect(() => {
-    sendApiRequest(
-      "GET",
-      "/discounts",
-      function (respone) {
-        setDiscounts(respone);
-      },
-      null,
-      function (error) {
-        console.error("Could not retrieve discounts: " + error);
-      }
-    );
-  }, []);
-
-  /**
-   * Tries to add product to API. If unsuccessfull an error message is
-   * display to the UI, if successfull a success message is display
-   * to the UI
-   */
   function handleSubmit(event) {
     event.preventDefault();
-    // Check valid fields
-    if (
-      name === "" ||
-      desc === "" ||
-      price === "" ||
-      category === "" ||
-      sex === "" ||
-      sizes === "" ||
-      quantity === "" ||
-      images.length === 0 ||
-      details === ""
-    ) {
-      displayFeedback(
-        "error",
-        "Make sure there are no empty fields",
-        document.querySelector("[data-submit-product]"),
-        document.querySelector("[data-feedback-product]")
-      );
-      // Make sure only images are uploaded
-    } else if (!isImages(images)) {
-      displayFeedback(
-        "error",
-        "Only jpg/jpeg and png accepted",
-        document.querySelector("[data-submit-product]"),
-        document.querySelector("[data-feedback-product]")
-      );
-    } else {
-      // TODO: transform images into binary
-      const newProduct = {
-        productName: name,
-        description: desc,
-        price: parseFloat(price),
-        category: category,
-        sex: sex,
-        colors: colors,
-        sizes: sizes,
-        quantity: quantity,
-        discount: discount,
-        details: details,
-      };
-      sendApiRequest(
-        "POST",
-        "/products",
-        function (response) {
-          displayFeedback(
-            "success",
-            "Product was added",
-            document.querySelector("[data-submit-product]"),
-            document.querySelector("[data-feedback-product]")
-          );
-          updateProducts();
-        },
-        newProduct,
-        function (error) {
-          displayFeedback(
-            "error",
-            "Something went wrong. Product was not added",
-            document.querySelector("[data-submit-product]"),
-            document.querySelector("[data-feedback-product]")
-          );
-        }
-      );
-    }
-  }
-
-  /**
-   * Sends a delete request trying to delete product with the id of
-   * the product to delete to the API.
-   * If unsuccessfull an error message is printed to console
-   */
-  function handleDeletion(event) {
-    const productId = event.target.value;
-    const relativeUrl = "/products/" + productId;
-    sendApiRequest("DELETE", relativeUrl, updateProducts, null, function () {
-      console.error("Could not delete product with id: " + productId);
-    });
+    console.log(title, colors, featured);
   }
 
   return (
     <section className="product-form box">
-      <form action="" className="form form--stretch">
+      <form onSubmit={handleSubmit}>
+        <h3 className="form__title form__title--small">Add product</h3>
         <fieldset className="form__section form__section--no-space form__section--horizontal">
-          <legend className="form__title form__title--small">
-            Add product
-          </legend>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-name" className="form__label">
-              Name
-            </label>
-            <input
-              id="product-name"
-              type="text"
-              className="form__input"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-desc" className="form__label">
-              Desc
-            </label>
-            <input
-              id="product-desc"
-              type="text"
-              className="form__input"
-              value={desc}
-              onChange={(e) => setDesc(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-price" className="form__label">
-              Price
-            </label>
-            <input
-              id="product-price"
-              type="number"
-              className="form__input"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-category" className="form__label">
-              Category
-            </label>
-            <input
-              id="product-category"
-              type="text"
-              className="form__input"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-sex" className="form__label">
-              Sex
-            </label>
-            <input
-              id="product-sex"
-              type="text"
-              className="form__input"
-              value={sex}
-              onChange={(e) => setSex(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-color" className="form__label">
-              Colors (separated by ", ")
-            </label>
-            <input
-              id="product-color"
-              type="text"
-              className="form__input"
-              value={colors}
-              onChange={(e) => setColors(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-sizes" className="form__label">
-              Sizes (separated by ", ")
-            </label>
-            <input
-              id="product-sizes"
-              type="text"
-              className="form__input"
-              value={sizes}
-              onChange={(e) => setSize(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-quantity" className="form__label">
-              Quantity
-            </label>
-            <input
-              id="product-quantity"
-              type="number"
-              className="form__input"
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-          </div>
-          <div className="form__input--wrapper">
-            <label htmlFor="product-discount" className="form__label">
-              Discount
-            </label>
-            <select
-              id="product-discount"
-              type="number"
-              className="form__input"
-              value={discount}
-              onChange={(e) => setDiscount(e.target.value)}
-            >
-              <option value="null">None</option>
-              {discounts.forEach((discount) => (
-                <option value={discount}>{discount}</option>
-              ))}
-            </select>
-          </div>
-          <div className="form__input--wrapper">
-            <label
-              htmlFor="product-images"
-              className="form__label custom-file-upload"
-            >
-              Upload images
-            </label>
-            <input
-              id="product-images"
-              type="file"
-              className="form__input"
-              accept="image/png, image/jpeg"
-              multiple
-              onChange={(e) => setImages(e.target.files)}
-            />
-          </div>
-          <div className="form__input--wrapper form__input--full-width">
-            <label htmlFor="product-details" className="form__label">
-              Product Details (separate with new line)
+          <FormInput
+            value={title}
+            updateValue={setTitle}
+            label="Product title"
+          />
+          <FormInput
+            value={description}
+            updateValue={setDescription}
+            label="Description"
+          />
+          <FormInput
+            value={price}
+            updateValue={setPrice}
+            label="Price"
+            type="number"
+          />
+          <FormInput
+            value={category}
+            updateValue={setCategory}
+            label="Category"
+          />
+          <FormInput value={sex} updateValue={setSex} label="Sex" />
+          <FormInput
+            value={discount}
+            updateValue={setDiscount}
+            label="Discount"
+            type="number"
+          />
+          <FormInput
+            value={colors}
+            updateValue={setColors}
+            label="Colors (Separated by ', ')"
+          />
+          <FormInput
+            value={sizes}
+            updateValue={setSizes}
+            label="Sizes (Separated by ', ')"
+          />
+          <FormInput
+            value={images}
+            updateValue={setImages}
+            label="Images"
+            type="file"
+          />
+          <FormInput
+            value={featured}
+            updateValue={setFeatured}
+            label="Featured"
+            type="checkbox"
+          />
+          <div className="form__input--wrapper form__input--wrapper--full-width">
+            <label htmlFor="details" className="form__label">
+              Product details (Separated with new line)
             </label>
             <textarea
-              rows="4"
-              id="product-details"
               className="form__input form__input--textarea"
-              value={details}
-              onChange={(e) => setDetails(e.target.value)}
+              id="details"
+              cols="30"
+              rows="5"
             ></textarea>
           </div>
         </fieldset>
-        <button
-          className="cta cta--tiny"
-          onClick={handleSubmit}
-          data-submit-product
-        >
+        <button className="cta cta--small" type="submit">
           Add product
         </button>
-        <p
-          className="form__feedback form__feedback--inline"
-          data-feedback-product
-        >
-          Failed to add product
-        </p>
       </form>
-      <table className="table">
-        <caption className="table__title">Products</caption>
-        <thead>
-          <tr>
-            <th className="table__heading">Id</th>
-            <th className="table__heading">Name</th>
-            <th className="table__heading">Description</th>
-            <th className="table__heading">Price</th>
-            <th className="table__heading">Category</th>
-            <th className="table__heading">Sex</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.length === 0 ? (
-            <tr>
-              <td>Loading...</td>
-            </tr>
-          ) : (
-            products.map((product) => (
-              <tr key={product.id}>
-                <td>{product.id}</td>
-                <td>{product.productName}</td>
-                <td>{product.description}</td>
-                <td>{product.price}</td>
-                <td>{product.category}</td>
-                <td>{product.sex}</td>
-                <td className="table__row--delete">
-                  <button onClick={handleDeletion} value={product.id}>
-                    X
-                  </button>
-                </td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
     </section>
   );
 }
