21d0ca7fc0dfb41612397af00a8cf4a554e19f33
jKm00 <joakied@stud.ntnu.no>
2022-05-20 23:45:47 +0200

    Made it work with multiple images and on mobile
    Had to remove vertical navigation. Dont know why but it fucked up everything

--- frontend/webapp/src/components/DescriptionBox.jsx
+++ frontend/webapp/src/components/DescriptionBox.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-
 // Import header styles
 import "../styles/descriptionBox.css";
 
-function DescriptionBox({ title, desc }) {
+function DescriptionBox({ details }) {
   return (
     <div className="description-box">
-    <h4 className="product__description__title">{title}</h4>
-    <p className="product__description">{desc}</p>
+      <h4 className="product__description__title">Details</h4>
+      <ul className="product__description__list">
+        <li className="product__description__item">Holds heat effectivly</li>
+        <li className="product__description__item">Very warm</li>
+        <li className="product__description__item">Much wow</li>
+        <li className="product__description__item">Such cool</li>
+      </ul>
     </div>
   );
 }
 
 export default DescriptionBox;
-
