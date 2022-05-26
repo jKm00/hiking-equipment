211fd734032c77624e31a9e0424cba7ecd5b99b4
Torstein <taurstein@gmail.com>
2022-05-26 22:37:38 +0200

    removed parameter from code to work with corresponding methods

--- frontend/webapp/src/tools/image-api-handler.js
+++ frontend/webapp/src/tools/image-api-handler.js
@@ -2,8 +2,8 @@ import {sendApiRequest} from "./request";
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-export function sendApiFileUploadRequest(url, fileContent, callback, errorCallback) {
-  sendApiRequest("post", url, callback, null, errorCallback, fileContent)
+export function sendApiFileUploadRequest(url, callback, errorCallback) {
+  sendApiRequest("post", url, callback, null, errorCallback)
 }
 
 export function sendApiDeleteRequest(url, callback, errorCallback) {
