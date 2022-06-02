114c6661f5aeabd6afd08b0365b8c811c4f54ed2
Eduard Andrei Cristea <falniir@gmail.com>
2022-06-02 15:10:53 +0200

    Added search for products

--- backend/src/main/java/no/ntnu/xxs/controllers/ProductController.java
+++ backend/src/main/java/no/ntnu/xxs/controllers/ProductController.java
@@ -9,13 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
- * REST API controller for product collection
- */
+* REST API controller for product collection
+*/
 // TODO: Make product endpoint public. This is made private only for demo purposes
 @RestController
 @RequestMapping("/api/products")
@@ -24,13 +22,13 @@ public class ProductController {
     private final String UNDEFINED = "undefined";
     @Autowired
     private ProductService productService;
-
+    
     /**
-     * Returns a list of all products with the attributes given.
-     * @param sex the sex of the products to be returned
-     * @param category the category of the products to be returned
-     * @return a list of all products
-     */
+    * Returns a list of all products with the attributes given.
+    * @param sex the sex of the products to be returned
+    * @param category the category of the products to be returned
+    * @return a list of all products
+    */
     @GetMapping
     @CrossOrigin
     public List<Product> getProductsBySexAndCategory(@RequestParam String sex, @RequestParam String category) {
@@ -46,12 +44,12 @@ public class ProductController {
         }
         return products;
     }
-
+    
     /**
-     * Returns an item with the id given in the URL
-     * @param id the id of the item to get
-     * @return an item with same id as given
-     */
+    * Returns an item with the id given in the URL
+    * @param id the id of the item to get
+    * @return an item with same id as given
+    */
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable long id) {
         ResponseEntity<Product> response;
@@ -63,45 +61,45 @@ public class ProductController {
         }
         return response;
     }
-
+    
     /**
     * HTTP POST request to /api/products. Tries to add product to database.
     * @param requestBody The body of the request containing the product to be added
     * @return Http.OK when product is added, or Http.CONFLICT if product
-     * is already registered
+    * is already registered
     */
     @PostMapping("/add")
     /* TODO: make it only accessible for admin. Currently its accessible for any
-        logged in users */
+    logged in users */
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> addProduct(@RequestBody AddProductRequest requestBody) {
         try {
             this.productService.addProduct(
-                    new Product(
-                            requestBody.getProductName(),
-                            requestBody.getDescription(),
-                            requestBody.getPrice(),
-                            requestBody.getCategory(),
-                            requestBody.getSex(),
-                            requestBody.isFeatured(),
-                            requestBody.getDiscount()),
-                    requestBody.getColors(),
-                    requestBody.getSizes(),
-                    requestBody.getDetails());
+            new Product(
+            requestBody.getProductName(),
+            requestBody.getDescription(),
+            requestBody.getPrice(),
+            requestBody.getCategory(),
+            requestBody.getSex(),
+            requestBody.isFeatured(),
+            requestBody.getDiscount()),
+            requestBody.getColors(),
+            requestBody.getSizes(),
+            requestBody.getDetails());
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (ProductAlreadyExistException e) {
             return new ResponseEntity<>("Product already exists", HttpStatus.CONFLICT);
         }
     }
-
+    
     /**
-     *  HTTP PUT request to /api/products/{id}
-     * @param id The id of the product to be updated
-     *
-     */
+    *  HTTP PUT request to /api/products/{id}
+    * @param id The id of the product to be updated
+    *
+    */
     @DeleteMapping("/{id}")
     /* TODO: make it only accessible for admin. Currently its accessible for any
-        logged in users */
+    logged in users */
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> deleteProduct(@PathVariable() Long id) {
         if (this.productService.deleteProduct(id)) {
@@ -109,4 +107,26 @@ public class ProductController {
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+
+
+
+
+
+/**
+ * Searches for products with the given name
+ * 
+ * @param search name of the product to search for
+ * @return 
+ */
+    @GetMapping("/search/{name}")
+    @CrossOrigin
+    public List<Product> searchProducts(@PathVariable String name) {
+
+        return this.productService.searchProducts(name);
+
+
+    }
+
+
+
 }
