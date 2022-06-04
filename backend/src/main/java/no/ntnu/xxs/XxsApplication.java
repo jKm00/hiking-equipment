2463e40be0045ea8cd11a3ce714e06c3ffc13453
Torstein <taurstein@gmail.com>
2022-06-04 22:06:47 +0200

    Added docket, limiting the documentation to our endpoints
    limits documentation to our endpoints, not all endpoints coming with spring boot

--- backend/src/main/java/no/ntnu/xxs/XxsApplication.java
+++ backend/src/main/java/no/ntnu/xxs/XxsApplication.java
@@ -2,6 +2,11 @@ package no.ntnu.xxs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class XxsApplication {
@@ -9,5 +14,19 @@ public class XxsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(XxsApplication.class, args);
 	}
+
+	@Bean
+	public Docket SwaggerConfiguration () {
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("no.ntnu.xxs"))
+				.build();
+
+
+	}
+
+
 
 }
