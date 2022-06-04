9d7fcbd069c052b26a7b37ebac832df4f2db5256
Torstein <taurstein@gmail.com>
2022-06-04 22:27:03 +0200

    added apiDetails()
    added apidetails containing metadata about the project, replacing default metadata from swagger

--- backend/src/main/java/no/ntnu/xxs/XxsApplication.java
+++ backend/src/main/java/no/ntnu/xxs/XxsApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class XxsApplication {
@@ -17,16 +20,26 @@ public class XxsApplication {
 
 	@Bean
 	public Docket SwaggerConfiguration () {
-
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.paths(PathSelectors.ant("/api/*"))
 				.apis(RequestHandlerSelectors.basePackage("no.ntnu.xxs"))
-				.build();
-
-
+				.build()
+				.apiInfo(apiDetails());
 	}
 
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+			"XXS - API",
+			"API for the XXS-project in application programming at ntnu",
+			"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Group 02", "gr02.appdev.cloudns.ph", "gr02@ntnu.no"),
+				"Api License",
+				"ntnu.no",
+				Collections.emptyList()
+				);
+	}
 
 
 }
