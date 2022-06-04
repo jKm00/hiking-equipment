dd079a4a5827b5358d97c472681ff57df6fc3bdd
Torstein <taurstein@gmail.com>
2022-06-04 18:15:10 +0200

    added permit all to swagger endpoints
    Added permitall to swagger endpoints, else the swagger-ui will never be available

--- backend/src/main/java/no/ntnu/xxs/security/SecurityConfiguration.java
+++ backend/src/main/java/no/ntnu/xxs/security/SecurityConfiguration.java
@@ -57,6 +57,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/signup").permitAll()
                 .antMatchers("/api/products/**").permitAll()
                 .antMatchers("/api/users/*").permitAll()
+                .antMatchers("/v3/api-docs").permitAll()
+                .antMatchers("/swagger-ui/").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/v3/api-docs").permitAll()
+                .antMatchers("/configuration/ui").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/configuration/security").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/swagger-ui/*").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/v3/**").permitAll()
+
 
                 // Every other endpoints need one form of authentication
                 // What type of authentication is specified with an annotation over each endpoint
