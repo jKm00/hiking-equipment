b4a058a06e275ef9ae0e1525fca0e620bad4d40b
Torstein <taurstein@gmail.com>
2022-06-05 23:49:08 +0200

    refactored order to display correct information from orders

--- frontend/webapp/src/components/Order.jsx
+++ frontend/webapp/src/components/Order.jsx
@@ -1,9 +1,10 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "../styles/order.css";
 import { sendApiRequest } from "../tools/request";
 
-export default function Order({ order, updateOrders}) {
+export default function Order({ order, updateOrders }) {
 
   function deleteOrder() {
     const relativeUrl = "/orders/delete/" + order.id;
@@ -17,33 +18,29 @@ export default function Order({ order, updateOrders}) {
       <div className="order__detail">
         <h3 className="order__detail__title">Products</h3>
         <ul className="order__detail__list">
-          {order.products.map((product) => (
-            <li key={product.id} className="order__detail__item">
-              {product.title}
-            </li>
-          ))}
+          {
+            order.orderItems.map(product => (
+              <li key={product.id} className="order__detail__item">
+              {product.productName}
+              </li>
+            ))
+          }
         </ul>
       </div>
       <div className="order__detail">
         <h3 className="order__detail__title">Order date</h3>
-        <p className="order__detail__body">{order.date}</p>
+        <p className="order__detail__body">{order.orderDate}</p>
       </div>
       <div className="order__detail">
         <h3 className="order__detail__title">Total sum</h3>
         <p className="order__detail__body">
-          {order.products
-            .map((product) => product.price)
-            .reduce(
-              (previousValue, currentValue) => previousValue + currentValue,
-              0
-            )}
-          ,-
+          {order.totalPrice},-
         </p>
       </div>
       <div className="order__detail">
-        <h3 className="order__detail__title">Payment status</h3>
+        <h3 className="order__detail__title">Order status</h3>
         <p className="order__detail__body">
-          {order.paymentStatus ? "Payed" : "Not payed"}
+          {order.status}
         </p>
       </div>
       <button onClick={deleteOrder} className="order__link">
